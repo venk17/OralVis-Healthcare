a91b375dc1b8f628cee19f1b7e3fb7225852316a
venk17 <k.venkatramanan2002@gamil.com>
2025-08-31 15:57:50 +0530

    backend url updated

--- oralvis-backend/server.js
+++ oralvis-backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', 'http://localhost:3000,https://oral-vis-healthcare.vercel.app/'],
   credentials: true
 }));
 app.use(express.json());
